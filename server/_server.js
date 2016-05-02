501c911ce1fb3ada7109d1398cfc7760957080b4
heidilaursen <heidimlaursen@gmail.com>
2016-05-02 15:39:01 -0700

    user router signup and signin

--- server/_server.js
+++ server/_server.js
@@ -3,11 +3,13 @@ const app = express();
 const mongoose = require('mongoose');
 const crimeRouter = require(__dirname + '/router/crime_router');
 const adminRouter = require(__dirname + '/router/admin_router');
+const userRouter = require(__dirname + '/router/user_router');
 // require router files here
 
 // app.use other routes here
 app.use('/api', adminRouter);
 app.use('/api', crimeRouter);
+app.use('/', userRouter);
 app.use('/*', (req, res) => {
   res.status(400).send('not found');
 });
--- server/router/user_router.js
+++ server/router/user_router.js
@@ -1,6 +1,58 @@
-'use strict'
+'use strict';
 
 const express = require('express');
-const User = require(__dirname + '/../model/user');
+const User = require(__dirname + '/../../model/user');
 const jsonParser = require('body-parser').json();
-const basicHTTP = requre(__dirname + '/../LIB/');
+const basicHTTP = require(__dirname + '/../../LIB/basic_http');
+
+var router = module.exports = exports = express.Router();
+
+router.post('/signup', jsonParser, (req, res) => {
+  var password = req.body.password;
+  req.body.password = null;
+
+  if (!password) {
+    return res.status(500).json({ msg: 'add a password' });
+  }
+
+  var newUser = new User(req.body);
+  newUser.generateHash(password);
+  password = null;
+
+  newUser.save((err, user) => {
+    if (err) {
+      return res.status(500).json({ msg: 'could not create user' });
+    }
+
+    user.generateToken(function(err, token) {
+      if (err) {
+        return res.status(500).json({ msg: 'could not generate token' });
+      }
+      res.json({ token });
+    });
+  });
+});
+
+router.get('/signin', basicHTTP, (req, res) => {
+  User.findOne({ username: req.auth.username }, (err, user) => {
+    if (err) {
+      return res.status(500).json({ msg: 'no authentication' });
+    }
+
+    if (!user) {
+      return res.status(500).json({ msg: 'no user' });
+    }
+
+    if (!user.compareHash(req.auth.password)) {
+      return res.status(500).json({ msg: 'could not authenticate' });
+    }
+
+    user.generateToken(function(err, token) {
+      if (err) {
+        return res.status(500).json({ msg: 'could not generate' });
+      }
+
+      res.json({ token });
+    });
+  });
+});
