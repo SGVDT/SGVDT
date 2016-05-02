b62c5f37e5f59d901c8f48ca280c5c6b0bc6327b
Shelly Yusuf <SputnikSeattle@Michelles-MacBook-Air.local>
2016-05-02 11:18:22 -0700

    adding folders and files for crime database functionality

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const mongoose = require('mongoose');
+
+const Crime = require(__dirname + '/../model/crime');
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/crimes_db');
+
+
+const crimeRouter = require(__dirname + '/router/crime_router');
+
+
+
+app.use('/api', crimeRouter);
+app.listen(PORT, () => console.log('server up on ' + PORT));
