e06cc01645b4b9fa5c6a91b2b07efc6f81f32a83
MARIA ROMERO <mariaromero18e@gmail.com>
2016-05-03 13:58:53 -0700

    update crime schema

--- model/crime.js
+++ model/crime.js
@@ -4,9 +4,12 @@ const mongoose = require('mongoose');
 var crimeSchema = new mongoose.Schema({
   offense: { type: String },
   date: { type: String },
+  day: { type: Number },
+  month: { type: Number },
+  year: { type: Number },
   time: { type: String },
   longitude: { type: Number },
-  lattitude: { type: Number },
+  latitude: { type: Number },
   zone: { type: String },
   rms_cdw_id: { type: String }
 });
