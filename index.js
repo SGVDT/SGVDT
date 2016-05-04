2149e07e0aff0b0ad6e3a0965a714bc8a4f784b3
heidilaursen <heidimlaursen@gmail.com>
2016-05-03 17:06:58 -0700

    playing with mongodb for heroku

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 module.exports = exports = require(__dirname + '/server/server');
+var uristring = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongod://localhost/HelloMongoose';
+
+var port = process.env.PORT || 5000;
+
+mongoose.connect(uristring, function(err, res) {
+  if (err) {
+    console.log('Error connecting to: ' + uristring + '.' + err);
+  } else {
+    console.log('Succeeded connecting to: ' + uristring);
+  }
+});
