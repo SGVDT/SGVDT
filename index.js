bf28d95e9240bb36c6129afa0c4dfe73fdfea662
heidilaursen <heidimlaursen@gmail.com>
2016-05-03 19:05:32 -0700

    trying to incorporate mongodb

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 module.exports = exports = require(__dirname + '/server/server');
+
+const mongoose = require('mongoose');
+const http = require('http');
+var port = process.env.PORT || 5000;
+
+
 var uristring = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongod://localhost/HelloMongoose';
 
-// const mongoose = require('mongoose');
-// const http = requre('http');
-// var port = process.env.PORT || 5000;
-//
-// mongoose.connect(uristring, function(err, res) {
-//   if (err) {
-//     console.log('Error connecting to: ' + uristring + '.' + err);
-//   } else {
-//     console.log('Succeeded connecting to: ' + uristring);
-//   }
-// });
+mongoose.connect(uristring, function(err, res) {
+  if (err) {
+    console.log('Error connecting to: ' + uristring + '.' + err);
+  } else {
+    console.log('Succeeded connecting to: ' + uristring);
+  }
+});
