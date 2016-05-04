b63be7f2f9a2caba457cc9b831cdf931b3e87e23
heidilaursen <heidimlaursen@gmail.com>
2016-05-03 17:24:58 -0700

    commented out crap for heroku

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 module.exports = exports = require(__dirname + '/server/server');
 var uristring = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongod://localhost/HelloMongoose';
 
-const mongoose = require('mongoose');
-const http = requre('http');
-var port = process.env.PORT || 5000;
-
-mongoose.connect(uristring, function(err, res) {
-  if (err) {
-    console.log('Error connecting to: ' + uristring + '.' + err);
-  } else {
-    console.log('Succeeded connecting to: ' + uristring);
-  }
-});
+// const mongoose = require('mongoose');
+// const http = requre('http');
+// var port = process.env.PORT || 5000;
+//
+// mongoose.connect(uristring, function(err, res) {
+//   if (err) {
+//     console.log('Error connecting to: ' + uristring + '.' + err);
+//   } else {
+//     console.log('Succeeded connecting to: ' + uristring);
+//   }
+// });
