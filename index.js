50f7208e4e491bd9a8939085fb16baf883c19f29
heidilaursen <heidimlaursen@gmail.com>
2016-05-04 11:58:47 -0700

    removing /hellomongoose crap code

--- index.js
+++ index.js
@@ -3,14 +3,3 @@ module.exports = exports = require(__dirname + '/server/server');
 const mongoose = require('mongoose');
 const http = require('http');
 var port = process.env.PORT || 5000;
-
-
-var uristring = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongod://localhost/HelloMongoose';
-
-mongoose.connect(uristring, function(err, res) {
-  if (err) {
-    console.log('Error connecting to: ' + uristring + '.' + err);
-  } else {
-    console.log('Succeeded connecting to: ' + uristring);
-  }
-});
