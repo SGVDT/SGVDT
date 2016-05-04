f6d3ac1285ca6300a4e0388c7a4bd5b2aa7201d0
MARIA ROMERO <mariaromero18e@gmail.com>
2016-05-04 15:34:52 -0700

    edit dbQueries

--- LIB/dbQueries.js
+++ LIB/dbQueries.js
@@ -2,27 +2,14 @@ const mongoose = require('mongoose');
 const Crime = require(__dirname + '/../model/crime');
 const errorHandler = require(__dirname + '/error_handler');
 
-var yesterday = {
- date: new Date(Date.now() - 8.64e+7),
- day: (new Date(Date.now() - 8.64e+7)).getDate(),
- month: (new Date(Date.now() - 8.64e+7)).getMonth() + 1,
- year: (new Date(Date.now() - 8.64e+7)).getFullYear()
-};
-
-var dayBefore = {
- date: new Date(Date.now() - 1.73e+8),
- day: (new Date(Date.now() - 1.73e+8)).getDate(),
- month: (new Date(Date.now() - 1.73e+8)).getMonth() + 1,
- year: (new Date(Date.now() - 1.73e+8)).getFullYear()
-};
-
 // by location
 // by year
 // by incident type
 var query = {
-  byYear: function(requestedYear) {
+  byYear: function(requestedYear, callback) {
     Crime.find({ year: requestedYear }, (err, data) => {
       if (err) return errorHandler(err);
+      callback(data);
       return data;
     });
   },
@@ -60,19 +47,11 @@ function byMonth(requestedMonth, requestedYear, callBack) {
     return data;
   });
 }
-
-byMonth(5, 2016, (data) => {
-  console.log('test' + data);
+query.byYear(2016, (data) => {
+  console.log('byYear array function ' + data);
 });
 
-// var updateArray.map( (collection) => {
-//     return collection.find({ continent: region }).exec();
-//   });
-//   Promise.all(promiseArray).then( (resultsArray) => {
-//     friends = resultsArray.reduce( (previous, current) => {
-//       return previous.concat(current);
-//     }, []);
-//     var text = displayFriends(region, friends);
-//     res.status(200).send(text);
-//   });
+
+// byMonth(5, 2016, (data) => {
+//   console.log('test' + data);
 // });
