5ed1dea528cde204820ffad99137b1f454fc1dd5
heidilaursen <heidimlaursen@gmail.com>
2016-05-03 17:22:27 -0700

    fixing heroku

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 module.exports = exports = require(__dirname + '/server/server');
 var uristring = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongod://localhost/HelloMongoose';
 
+const mongoose = require('mongoose');
+const http = requre('http');
 var port = process.env.PORT || 5000;
 
 mongoose.connect(uristring, function(err, res) {
