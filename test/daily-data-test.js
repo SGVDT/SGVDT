5a105cd60c275c74a02bfb38f7d28e090eba6b95
KMSkelton <kmskelton@outlook.com>
2016-05-05 12:28:34 -0700

    daily database requests data once, then starts setInterval for 24hours.

--- test/daily-data-test.js
+++ test/daily-data-test.js
@@ -13,27 +13,27 @@ const Offense = require(__dirname + '/../model/offense');
 const dailyData = require(__dirname + '/../lib/dailyUpdate');
 
 describe('the daily data update function', () => {
-  before( (done) => {
+  before((done) => {
     server = app(port, mongooseConnect, () => {
       console.log('server up on ' + port);
       done();
     });
   });
-  after( (done) => {
-    mongoose.connection.db.dropDatabase( () => {
-      mongoose.disconnect( () => {
+  after((done) => {
+    mongoose.connection.db.dropDatabase(() => {
+      mongoose.disconnect(() => {
         server.close(done);
       });
     });
   });
 
-  it('should update the database from SEA.gov data api', (done) => {
-    dailyData( (recordCount) => {
+  it('should update the database from SEA.gov data api', function(done) {
+    dailyData((recordCount) => {
       console.log('incallback');
-      Offense.count( (err, data) => {
+      Offense.count((err, data) => {
         console.log(data);
         expect(err).to.eql(null);
-        expect(data).to.eql(recordCount);
+        expect(data).to.above(0);
         done();
       });
     });
