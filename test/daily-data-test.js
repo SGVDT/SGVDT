0f8292c4441a77b66d466d7c0a9a5611294b19bf
KMSkelton <kmskelton@outlook.com>
2016-05-04 17:20:51 -0700

    not getting on gitcommitsfromlastnight

--- test/daily-data-test.js
+++ test/daily-data-test.js
@@ -5,18 +5,25 @@ chai.use(chaiHttp);
 const mongoose = require('mongoose');
 const request = chai.request;
 const Crime = require(__dirname + '/../model/crime');
-const dailyData = require(__dirname + '/../server/router/daily_router');
+require(__dirname + '/../server/router/daily_router');
 // the following order is important:
 const port = process.env.PORT = 11235;
 process.env.MONGO_URI = 'mongodb://localhost/admin_test_db';
 var app = require(__dirname + '/../server/_server');
+var server;
 
 describe('the daily data pull', () => {
   before((done) => {
-    app(port, process.env.MONGO_URI, () => {
+    server = app(port, process.env.MONGO_URI, () => {
       console.log('server up on ' + port);
-      dailyData.dailyData();
       done();
+    });
+  });
+  after(function(done) {
+    mongoose.connection.db.dropDatabase(() => {
+      mongoose.disconnect(() => {
+        server.close(done);
+      });
     });
   });
   it('should GET all the crimes', (done) => {
