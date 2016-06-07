2c9ecba7ea6ac51653bec7a02161b49fd6f0785a
Maria <MiaRomero@users.noreply.github.com>
2016-06-07 10:08:17 -0700

    Update newsAPI.js

--- front_end/app/js/services/newsAPI.js
+++ front_end/app/js/services/newsAPI.js
@@ -1,7 +1,7 @@
 
 
 
-https://gateway-a.watsonplatform.net/calls/data/GetNews?outputMode=json&return=enriched.url.url,enriched.url.title,enriched.url.publicationDate.date,enriched.url.image,enriched.url.text&start=now-30d&end=now&q.enriched.url.title=A[seattle^gun]&q.enriched.url.text=A[seattle^gun^violence]&return=enriched.url.title&apikey=a6fa1fcadfacf88a58533d8fb2bdc12e6330a235
+https://gateway-a.watsonplatform.net/calls/data/GetNews?outputMode=json&return=enriched.url.url,enriched.url.title,enriched.url.publicationDate.date,enriched.url.image,enriched.url.text&start=now-30d&end=now&q.enriched.url.title=A[seattle^gun]&q.enriched.url.text=A[seattle^gun^violence]&return=enriched.url.title&apikey=
 
 
 https://api.twitter.com/1.1/search/tweets.json?q=seattle%20gun%20violence%20lang%3Aen%20since%3A2016-05-23%20until%3A2016-06-06&src=typd&lang=en
