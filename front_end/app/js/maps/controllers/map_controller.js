58ec800c904b9366d54565ddaefed4b7e85313a2
Shelly Yusuf <SputnikSeattle@Michelles-MacBook-Air-2.local>
2016-06-06 23:11:26 -0700

    changed long lat to CF

--- front_end/app/js/maps/controllers/map_controller.js
+++ front_end/app/js/maps/controllers/map_controller.js
@@ -10,13 +10,14 @@ const lodash = require('lodash');
 // uiGmapgoogle-maps
 module.exports = function(app) {
     app.controller('MapController', function($scope) {
-
-    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
-
-
-    // uiGmapGoogleMapApi.then(function(maps) {
-    //   console.log('4:31 and now I learn about maps.');
-    // });
-
+    $scope.map = { center: { latitude: 47.618217, longitude: -122.351832 }, zoom: 8 };
     });
 };
+
+
+
+// (47.618217, -122.351832);
+
+// uiGmapGoogleMapApi.then(function(maps) {
+//   console.log('4:31 and now I learn about maps.');
+// });
