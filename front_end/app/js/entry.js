dd07b536fba9ee381ebc154c34ed4a134c5aa784
Aaron Filson <aaronfilson@gmail.com>
2016-06-06 17:02:48 -0700

    sample code on google maps for angular

--- front_end/app/js/entry.js
+++ front_end/app/js/entry.js
@@ -21,9 +21,17 @@ $rp
 .when('/map', {
     templateUrl: 'templates/maps/views/map_view.html',
     controller: 'MapController',
-    controllerAs: 'mapctrl'
+    controllerAs: 'supermapctrl'
 })
 .otherwise({
-    redirectTo: '/map'
+    redirectTo: '/offenses'
 });
 }]);
+
+sgvdtApp.config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+        //    key: 'your api key',
+        v: '3.20', //defaults to latest 3.X anyhow
+        libraries: 'weather,geometry,visualization'
+    });
+});
--- front_end/app/js/maps/controllers/map_controller.js
+++ front_end/app/js/maps/controllers/map_controller.js
@@ -3,8 +3,13 @@ const angmaps = require('angular-google-maps');
 const lodash = require('lodash');
 
 module.exports = function(app) {
-    app.controller('MapController', function($scope) {
+    app.controller('MapController', function($scope, uiGmapGoogleMapApi) {
+      debugger;
     $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+
+    uiGmapGoogleMapApi.then(function(maps) {
+      console.log('4:31 and now I learn about maps.');
+    });
     });
 };
 
