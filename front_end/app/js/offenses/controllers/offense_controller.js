751cd32c082f0b219be7c9eadc4d0f52b5affa0d
Shelly Yusuf <SputnikSeattle@Michelles-MacBook-Air-2.local>
2016-06-08 22:24:05 -0700

    changing the offense list to an offense table with filtering

--- front_end/app/js/offenses/controllers/offense_controller.js
+++ front_end/app/js/offenses/controllers/offense_controller.js
@@ -1,12 +1,37 @@
 
-var baseUrl = require('../../config').baseUrl;
-
 module.exports = function(app) {
-  app.controller('OffenseController', ['sgvdtResource', function(Resource) {
-    this.offenses = [];
-    this.errors = [];
-    var remote = new Resource(this.offenses, this.errors, baseUrl + '/api/offenses', { errMessages: { getAll: 'custome error message' } });
-    this.getAll = remote.getAll.bind(remote);
-    this.getAll();
+  app.controller('OffenseController', ['$scope', 'offenseResource',
+   function( $scope, Resource) {
+
+    $scope.offenseArr = [];
+    console.log($scope.offenseArr);
+    $scope.getDataList = function() {
+    $scope.offenseArr = Resource.getDataList();
+    };
+
+    $scope.setCurrentData = function(year) {
+
+        Resource.getIssue({
+            year: year
+        }, function(data) {
+            $scope.offenseArr = data;
+
+        });
+    };
+
+    $scope.getDataList();
+
   }]);
+
 };
+
+
+// module.exports = function(app) {
+//   app.controller('OffenseController', ['sgvdtResource', function(Resource) {
+//     this.offenses = [];
+//     this.errors = [];
+//     var remote = new Resource(this.offenses, this.errors, baseUrl + '/api/offenses', { errMessages: { getAll: 'custome error message' } });
+//     this.getAll = remote.getAll.bind(remote);
+//     this.getAll();
+//   }]);
+// };
--- front_end/app/js/services/index.js
+++ front_end/app/js/services/index.js
@@ -1,5 +1,6 @@
 module.exports = function(app) {
   require('./handle_error')(app);
   require('./sgvdt_resource')(app);
+  require('./offense_resource')(app);
   require('./news_resource')(app);
 };
