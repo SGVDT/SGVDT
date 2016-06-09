5ebff82b5c723b00581d44b548f2949757114615
Shelly Yusuf <SputnikSeattle@Michelles-MacBook-Air-2.local>
2016-06-09 13:07:06 -0700

    changed offense api router back to being able to include offense or offenses. Hoping to be able to put queries in later

--- server/router/offenses_router.js
+++ server/router/offenses_router.js
@@ -3,18 +3,18 @@ const Offense = require(__dirname + '/../model/offense');
 const eH = require(__dirname + '/../lib/error_handler');
 var offenseRouter = module.exports = exports = Router();
 
-// offenseRouter.get(/offense/, (req, res) => {
-//   Offense.find( req.query, (err, data) => {
-//     if (err) return eH(err, res);
-//     res.status(200).json(data);
-//   });
-
-  offenseRouter.get('/offenses', (req, res) => {
-      Offense.find(null, (err, data) => {
-          if (err) return eH(err, res);
-          res.status(200).json(data);
-      });
+offenseRouter.get(/offense/, (req, res) => {
+  Offense.find( req.query, (err, data) => {
+    if (err) return eH(err, res);
+    res.status(200).json(data);
   });
 
+  // offenseRouter.get('/offenses', (req, res) => {
+  //     Offense.find(null, (err, data) => {
+  //         if (err) return eH(err, res);
+  //         res.status(200).json(data);
+  //     });
+  // });
 
-// });
+
+ });
