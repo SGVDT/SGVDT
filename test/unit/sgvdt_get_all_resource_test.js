f694fba055188a68032b6c7a1022418f2485e19a
Shelly Yusuf <SputnikSeattle@Michelles-MacBook-Air-2.local>
2016-06-10 00:45:57 -0700

    adding offense resource factory service test

---
+++ test/unit/sgvdt_get_all_resource_test.js
@@ -0,0 +1,41 @@
+const angular = require('angular');
+
+describe('it should get the offenses', function() {
+  var $httpBackend;
+  var offensectrl;
+  beforeEach(angular.mock.module('sgvdtApp'));
+  beforeEach(angular.mock.inject((_$httpBackend_) => {
+    $httpBackend = _$httpBackend_;
+  }));
+
+  afterEach(() => {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
+  it('should get the mug resources', angular.mock.inject(function(sgvdtResource) {
+    $httpBackend.expectGET('http://localhost:3000/api/offenses').respond(200, [{ summary: 'Big Ben' }]);
+    var resourceArray = [{}, {}, {}];
+    var errorsArray = [];
+    var resource = new sgvdtResource(resourceArray, errorsArray, 'http://localhost:3000/api/offenses');
+    resource.getAll();
+    $httpBackend.flush();
+    expect(resourceArray.length).toBe(1);
+    expect(resourceArray[0].summary).toBe('Big Ben');
+  }));
+  // /
+  // it('should get the vinyl resources', angular.mock.inject(function(mvResource) {
+  //   $httpBackend.expectGET('http://localhost:5000/api/vinyl').respond(200, [{ album: 'III' }]);
+  //   var resourceArray = [{}, {}, {}];
+  //   var errorsArray = [];
+  //   var resource = new mvResource(resourceArray, errorsArray, 'http://localhost:5000/api/vinyl');
+  //   resource.getAll();
+  //   $httpBackend.flush();
+  //   expect(resourceArray.length).toBe(1);
+  //   expect(resourceArray[0].album).toBe('III');
+  // }));
+  // /
+
+
+
+});
